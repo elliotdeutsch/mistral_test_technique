e2da6d6becc9f4d9ec343a5863ca11c46fd5dbbc
Elliot Deutsch <elliot.deutsch@gmail.com>
2024-10-09 19:31:34 +0200

    Add build files

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7IDFTTBX.mjs";import"./chunk-42FBC5FI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-NWSG2CAR.mjs";import"./chunk-42FBC5FI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
